71124f7b65dbf3186e3c8ddf5c7a5c4595b16904
milecubela <mile.cubela@gmail.com>
2022-03-08 09:09:01 +0100

    Started database migration, maybe

--- src/main/resources/db/migration/V1_createMessageTable.sql
+++ src/main/resources/db/migration/V1_createMessageTable.sql
@@ -5,3 +5,35 @@ CREATE TABLE messages
     text varchar(50) NOT NULL,
         PRIMARY KEY(id)
 )
+
+-- CREATE TABLE messages
+-- (
+--     messageId bigint NOT NULL,
+--     title varchar(50) NOT NULL,
+--     text varchar(50) NOT NULL,
+--     PRIMARY KEY(messageId)
+-- );
+--
+-- CREATE  TABLE schedules
+-- (
+--     scheduleId bigint NOT NULL,
+--     runDate DATE NOT NULL,
+--     isRepeat BOOLEAN NOT NULL,
+--     isActive BOOLEAN NOT NULL,
+--     createdAt DATE NOT NULL,
+--     messageId bigint NOT NULL,
+--     PRIMARY KEY (scheduleId),
+--     FOREIGN KEY (messageId) REFERENCES messages(messageId)
+-- );
+--
+-- CREATE TABLE triggers
+-- (
+--     triggerId bigint NOT NULL,
+--     messageId bigint NOT NULL,
+--     channel varchar(50) NOT NULL,
+--     isActive BOOLEAN NOT NULL,
+--     createdAT DATE NOT NULL,
+--     CONSTRAINT PRIMARY KEY (triggerId),
+--     FOREIGN KEY (messageId) REFERENCES messages(messageId)
+-- )
+--
