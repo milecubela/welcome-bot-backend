ded003edd8d090c65f5a45b3213e3903952793e1
NDz <nikola_dzida2@hotmail.com>
2022-03-08 08:45:48 -0500

    Messages entity completed, application properties namjesten da vraca message na error

--- src/main/java/com/nsoft/welcomebot/Controllers/MessageController.java
+++ src/main/java/com/nsoft/welcomebot/Controllers/MessageController.java
@@ -5,7 +5,9 @@ import com.nsoft.welcomebot.Services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/v1/messages")
@@ -23,9 +25,18 @@ public class MessageController {
         return _messageService.getMessages();
     }
 
-    @PostMapping
-    public void createMessage(@RequestBody Message message) {
-        _messageService.createNewMessage(message);
+    @GetMapping(path = "{messageId}")
+    public Optional<Message> getMessages(@PathVariable("messageId") long messageId)  {
+        return _messageService.getMessageById(messageId);
     }
 
+    @PostMapping
+    public void createMessage(@Valid @RequestBody Message message) {
+        _messageService.createNewMessage(message);
+    }
+    //path varijable moraju imati identicno ime kao varijable koje se u funkciju pozivaju
+    @DeleteMapping(path = "{messageId}")
+    public void deleteStudent(@PathVariable("messageId") long messageId){
+        _messageService.deleteMessage(messageId);
+    }
 }
