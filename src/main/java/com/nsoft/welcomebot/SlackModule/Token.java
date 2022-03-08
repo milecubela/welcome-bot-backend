e63220f834b78b6ebcdd14d378981fac4d0066fb
tare99 <tare.arnaut@gmail.com>
2022-03-08 08:13:14 -0500

    Added SlackModule with SlackModel class and temporary Token class with the BOT Token and added Utilites folder with an enum for certain events

---
+++ src/main/java/com/nsoft/welcomebot/SlackModule/Token.java
@@ -0,0 +1,5 @@
+package com.nsoft.welcomebot.SlackModule;
+
+public class Token {
+    static public String SLACK_BOT_TOKEN = "xoxb-3185202762819-3200086523987-I0YyDa1Ahc2Avo12tpnhykCj";
+}
---
+++ src/main/java/com/nsoft/welcomebot/Utilities/TriggerTypesEnum.java
@@ -0,0 +1,3 @@
+package com.nsoft.welcomebot.Utilities;
+
+public enum TriggerTypesEnum {CHANNEL_LEFT,CHANNEL_JOINED,MESSSAGE_SENT}
