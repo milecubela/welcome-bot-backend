3c95fe8045fdd1c920fc32e070d434d2f05fa758
milecubela <mile.cubela@gmail.com>
2022-03-11 10:12:19 +0100

    Create post route for tokens
    Created a new ApiResponse Class, with HttpStatus, Message and payload, to standardize our http resposnes

--- src/main/java/com/nsoft/welcomebot/Controllers/AuthController.java
+++ src/main/java/com/nsoft/welcomebot/Controllers/AuthController.java
@@ -1,6 +1,10 @@
 package com.nsoft.welcomebot.Controllers;
 
 import com.nsoft.welcomebot.Repositories.UserRepository;
+import com.nsoft.welcomebot.Utilities.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/validateToken")
 public class AuthController {
 
+    @Autowired
     private UserRepository _userRepository;
+
+    @PostMapping
+    public ApiResponse verifyToken() {
+        return new ApiResponse(HttpStatus.OK, "SUCCESS");
+    }
 }
--- src/main/java/com/nsoft/welcomebot/Repositories/UserRepository.java
+++ src/main/java/com/nsoft/welcomebot/Repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package com.nsoft.welcomebot.Repositories;
 
+import com.nsoft.welcomebot.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
