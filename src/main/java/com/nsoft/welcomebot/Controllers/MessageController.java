cbc2200cd99681f4339bc5809e50a009c7242d09
milecubela <mile.cubela@gmail.com>
2022-03-22 09:05:16 +0100

    @AllArgsConstructor

--- src/main/java/com/nsoft/welcomebot/Controllers/MessageController.java
+++ src/main/java/com/nsoft/welcomebot/Controllers/MessageController.java
@@ -4,7 +4,6 @@ import com.nsoft.welcomebot.Entities.Message;
 import com.nsoft.welcomebot.Models.RequestModels.MessageRequest;
 import com.nsoft.welcomebot.Services.MessageService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
