31acb7b080abc1994913697b8d60dc8c0ec07ec3
milecubela <mile.cubela@gmail.com>
2022-03-30 11:49:04 +0200

    Started joke of the day command

--- src/main/java/com/nsoft/welcomebot/Utilities/SlackCommand.java
+++ src/main/java/com/nsoft/welcomebot/Utilities/SlackCommand.java
@@ -1,3 +1,3 @@
 package com.nsoft.welcomebot.Utilities;
 
-public enum SlackCommand {CHANNEL_INFO, WEATHER, TARIK, NIKOLA, MILE, SALKO, SELMA}
+public enum SlackCommand {CHANNEL_INFO, WEATHER, TARIK, NIKOLA, MILE, SALKO, SELMA, JOKE_OF_THE_DAY}
