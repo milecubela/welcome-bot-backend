703be139449cfe786d39548bebfb4125a64511bf
tare99 <tare.arnaut@gmail.com>
2022-03-31 12:46:47 +0200

    added testcontainer integration tests for trigger controller and trigger service

--- src/main/java/com/nsoft/welcomebot/WelcomeBotApplication.java
+++ src/main/java/com/nsoft/welcomebot/WelcomeBotApplication.java
@@ -6,16 +6,13 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.websocket.DeploymentException;
-import java.io.IOException;
-
 @SpringBootApplication
 @EnableSwagger2
 @ServletComponentScan
 @EnableScheduling
 public class WelcomeBotApplication {
 
-    public static void main(String[] args) throws DeploymentException, IOException {
+    public static void main(String[] args) {
         SpringApplication.run(WelcomeBotApplication.class, args);
     }
 
