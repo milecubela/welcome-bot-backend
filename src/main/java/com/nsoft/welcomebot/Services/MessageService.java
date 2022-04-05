bdaa2e0a4ec6d45ed58a792da287a07de619d0d9
milecubela <mile.cubela@gmail.com>
2022-04-05 14:03:21 +0200

    Started custom loggers

--- src/main/java/com/nsoft/welcomebot/Services/MessageService.java
+++ src/main/java/com/nsoft/welcomebot/Services/MessageService.java
@@ -1,10 +1,12 @@
 package com.nsoft.welcomebot.Services;
 
+import ch.qos.logback.classic.Logger;
 import com.nsoft.welcomebot.Entities.Message;
 import com.nsoft.welcomebot.ExceptionHandlers.CustomExceptions.NotFoundException;
 import com.nsoft.welcomebot.Models.RequestModels.MessageRequest;
 import com.nsoft.welcomebot.Repositories.MessageRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,11 +16,12 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-@Slf4j
+//@Slf4j
 @Service
 public class MessageService {
 
     private final MessageRepository _messageRepository;
+    Logger log = (Logger) LoggerFactory.getLogger("welcome.bot");
 
     @Autowired
     public MessageService(MessageRepository messageRepository) {
@@ -32,7 +35,7 @@ public class MessageService {
     public Message getMessageById(Long messageId) {
         Optional<Message> optionalMessage = _messageRepository.findById(messageId);
         if (optionalMessage.isEmpty()) {
-            log.error("Message with ID {} not found", messageId);
+            log.info("Message with ID {} not found", messageId);
             throw new NotFoundException("Message with ID " + messageId + " not found!");
         }
         return optionalMessage.get();
