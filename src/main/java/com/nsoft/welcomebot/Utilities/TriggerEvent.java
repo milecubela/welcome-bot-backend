47115ddabe4f6c2fc7282f2933d6ee216c24cb5f
milecubela <mile.cubela@gmail.com>
2022-04-05 11:20:14 +0200

    New event for reaction_added

--- src/main/java/com/nsoft/welcomebot/Utilities/TriggerEvent.java
+++ src/main/java/com/nsoft/welcomebot/Utilities/TriggerEvent.java
@@ -1,3 +1,3 @@
 package com.nsoft.welcomebot.Utilities;
 
-public enum TriggerEvent {CHANNEL_LEFT,CHANNEL_JOINED,APP_MENTION_EVENT,DND_UPDATED_USER}
+public enum TriggerEvent {CHANNEL_LEFT,CHANNEL_JOINED,APP_MENTION_EVENT,REACTION_ADDED}
