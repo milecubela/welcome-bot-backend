3d48f2b3fc94c8776ffdc5ff65db5936400835e3
milecubela <mile.cubela@gmail.com>
2022-04-12 13:35:33 +0200

    Some warnings

--- src/test/java/com/nsoft/welcomebot/IntegrationTests/Repositories/UserRepositoryIT.java
+++ src/test/java/com/nsoft/welcomebot/IntegrationTests/Repositories/UserRepositoryIT.java
@@ -4,7 +4,6 @@ import com.nsoft.welcomebot.Entities.User;
 import com.nsoft.welcomebot.Models.RequestModels.UserRequest;
 import com.nsoft.welcomebot.Repositories.UserRepository;
 import com.nsoft.welcomebot.Security.AuthUtils.UserRole;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
