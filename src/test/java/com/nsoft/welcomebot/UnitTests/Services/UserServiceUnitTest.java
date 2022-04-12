74a2fedc12a66b3ef2ac970c6b929baa0a1e9ef2
milecubela <mile.cubela@gmail.com>
2022-04-12 10:12:43 +0200

    Exceptions unit testing

--- src/test/java/com/nsoft/welcomebot/UnitTests/Services/UserServiceUnitTest.java
+++ src/test/java/com/nsoft/welcomebot/UnitTests/Services/UserServiceUnitTest.java
@@ -1,6 +1,10 @@
 package com.nsoft.welcomebot.UnitTests.Services;
 
+import com.google.gson.JsonObject;
 import com.nsoft.welcomebot.Entities.User;
+import com.nsoft.welcomebot.ExceptionHandlers.CustomExceptions.BadTokenException;
+import com.nsoft.welcomebot.Models.RequestModels.TokenRequest;
+import com.nsoft.welcomebot.Models.RequestModels.UserRequest;
 import com.nsoft.welcomebot.Repositories.UserRepository;
 import com.nsoft.welcomebot.Security.AuthUtils.UserRole;
 import com.nsoft.welcomebot.Services.OauthTokenService;
@@ -10,10 +14,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import javax.persistence.EntityExistsException;
+import java.io.IOException;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 
 //@SpringBootTest
@@ -32,6 +40,9 @@ class UserServiceUnitTest {
         userService = new UserService(userRepository, oauthTokenService);
     }
 
+    /**
+     * Test if getUserByEmail returns the user if it's present in database
+     */
     @Test
     void shouldValidateAndReturnUser() {
         //given
@@ -43,6 +54,9 @@ class UserServiceUnitTest {
         assertThat(result.get().getEmail()).isEqualTo(EMAIL);
     }
 
+    /**
+     * Testing if getUserByEmail returns emtpy if user doesn't exist
+     */
     @Test
     void shouldReturnEmptyIfUserDoesntExist(){
         //given
@@ -52,5 +66,64 @@ class UserServiceUnitTest {
         Optional<User> result = userService.getUserByEmail(EMAIL);
         //then
         assertThat(result).isEmpty();
+    }
+
+    /**
+     * Testing if addUser() will throw exception when user with given email already exists
+     */
+    @Test
+    void shouldThrowIfUserAlreadyExistsOnCreation(){
+        //given
+        User user = new User(1L, EMAIL, UserRole.ADMIN);
+        UserRequest userRequest = new UserRequest(EMAIL);
+        given(userService.getUserByEmail(EMAIL)).willReturn(Optional.of(user));
+        //then
+        assertThatThrownBy(() -> userService.addUser(userRequest)).isInstanceOf(EntityExistsException.class).hasMessageContaining("User with email : " + EMAIL + " already exists");
+    }
+
+    /**
+     * Testing if loginUser() throws if we recieve bad token request
+     */
+    @Test
+    void shouldThrowIfBadRequestTokenDoesntExist(){
+        //given
+        TokenRequest tokenRequest = new TokenRequest();
+        //then
+        assertThatThrownBy(() -> userService.loginUser(tokenRequest)).isInstanceOf(BadTokenException.class).hasMessageContaining("Bad Token request! Provide a bearer token");
+    }
+    /**
+     * Testing if loginUser() throws if we didn't define user in database
+     */
+    @Test
+    void shouldThrowIfUserDoesntExist() throws IOException {
+        //given
+        TokenRequest tokenRequest = new TokenRequest("accesstokenstring");
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("email", EMAIL);
+        given(oauthTokenService.verifyGoogleToken("accesstokenstring")).willReturn(jsonObject);
+        //then
+        assertThatThrownBy(() -> userService.loginUser(tokenRequest)).isInstanceOf(UsernameNotFoundException.class).hasMessageContaining("User doesn't exist in the database");
+    }
+    /**
+     * Testing if logoutUser() throws if we recieve bad token request
+     */
+    @Test
+    void shouldThrowIfBadRequestTokenDoesntExistOnLogout(){
+        //given
+        TokenRequest tokenRequest = new TokenRequest();
+        //then
+        assertThatThrownBy(() -> userService.logoutUser(tokenRequest)).isInstanceOf(BadTokenException.class).hasMessageContaining("Bad Token request! Provide a bearer token");
+    }
+
+    /**
+     * Testing if getEmailFromToken() will throw if google rejects the token
+     */
+    @Test
+    void shouldThrowIfInvalidGoogleTokenSent() throws IOException {
+        //given
+        String token = "accesstoken";
+        given(oauthTokenService.verifyGoogleToken(token)).willThrow(IOException.class);
+        //then
+        assertThatThrownBy(() -> userService.getEmailFromToken(token)).isInstanceOf(BadTokenException.class).hasMessageContaining("Invalid Google Token!");
     }
 }
